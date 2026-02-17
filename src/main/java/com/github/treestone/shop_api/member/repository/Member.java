43df2fbfc6d553fcfc918701976d58c5c825c549
남우석 <hinyong22@gmail.com>
2026-02-18 00:33:09 +0900

    [feat] member 추가

---
+++ src/main/java/com/github/treestone/shop_api/member/repository/Member.java
@@ -0,0 +1,14 @@
+package com.github.treestone.shop_api.member.repository;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+	String name;
+}

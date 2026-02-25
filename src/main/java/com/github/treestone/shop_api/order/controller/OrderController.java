a2588dda17664ffbce123c97bf83cd87cf3aea0d
남우석 <hinyong22@gmail.com>
2026-02-25 16:21:17 +0900

    Revert "[fix] refactor/db-lock PR리뷰 수정 (2)"
    This reverts commit ca8b0fb3509a5824d008b3af2344c3402c618250.

--- src/main/java/com/github/treestone/shop_api/order/controller/OrderController.java
+++ src/main/java/com/github/treestone/shop_api/order/controller/OrderController.java
@@ -1,7 +1,10 @@
 package com.github.treestone.shop_api.order.controller;
 
+import com.github.treestone.shop_api.inventory.usecase.DecreaseStockUseCase;
+import com.github.treestone.shop_api.inventory.usecase.DecreaseStockWithOptimisticLockUseCase;
+import com.github.treestone.shop_api.inventory.usecase.DecreaseStockWithPessimisticLockUseCase;
 import com.github.treestone.shop_api.order.controller.request.CreateOrderBody;
-import com.github.treestone.shop_api.order.processor.OrderProcessor;
+import com.github.treestone.shop_api.order.usecase.CreateOrderUseCase;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +17,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/order")
 @RequiredArgsConstructor
 public class OrderController {
-	private final OrderProcessor orderProcessor;
+	private final CreateOrderUseCase createOrderUseCase;
+	private final DecreaseStockUseCase decreaseStockUseCase;
+	private final DecreaseStockWithOptimisticLockUseCase decreaseStockWithOptimisticLockUseCase;
+	private final DecreaseStockWithPessimisticLockUseCase decreaseStockWithPessimisticLockUseCase;
 
 	@PostMapping
 	public ResponseEntity<Void> createOrder(@Valid @RequestBody CreateOrderBody body) {
-		orderProcessor.createOrderWithStockDecrease(body.productId(), body.userId());
+//		DecreaseStockUseCase.Output output = decreaseStockUseCase.execute(
+//				new DecreaseStockUseCase.Input(body.productId())
+//		);
+
+		DecreaseStockWithOptimisticLockUseCase.Output output = decreaseStockWithOptimisticLockUseCase.execute(
+				new DecreaseStockWithOptimisticLockUseCase.Input(body.productId())
+		);
+
+//		DecreaseStockWithPessimisticLockUseCase.Output output = decreaseStockWithPessimisticLockUseCase.execute(
+//				new DecreaseStockWithPessimisticLockUseCase.Input(body.productId())
+//		);
+
+		CreateOrderUseCase.Input createOrderUseCaseInput = new CreateOrderUseCase.Input(
+				body.userId(),
+				output.product()
+		);
+		createOrderUseCase.execute(createOrderUseCaseInput);
 		return ResponseEntity.ok().build();
 	}
 
